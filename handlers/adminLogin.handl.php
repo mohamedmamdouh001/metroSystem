fd0730d6985bc3b98874ea60ed84c2a450e8dc56
amino9 <133016965+amino9@users.noreply.github.com>
2023-05-09 16:38:23 +0300

    admin login

---
+++ handlers/adminLogin.handl.php
@@ -0,0 +1,42 @@
+
+<?php
+include '../config/config.php'
+?>
+<?php
+session_start();
+if (isset($_POST['systemLogin']))
+
+{
+      $id    = ($_POST['admin_id']);
+   $password = ( $_POST['password']);
+
+$stmt = "select * from `admin` where admin_id='$id'&& password='$password'";
+$res= mysqli_query($conn,$stmt);
+$row= mysqli_fetch_assoc($res);
+
+ if (mysqli_num_rows ($res)===1)
+  {
+    
+  if (($row["role"]) =="system_admin"){
+
+ echo "you are system admin in our system";
+  }
+
+ elseif( ($row["role"])=="metro_admin"){
+  echo" you are metro admin check requests";
+ }
+
+else if (($row["role"])=="education_admin")
+    { 
+            echo  "you are education_admin valdiate request";
+}
+
+
+
+}
+else
+{
+ echo'wrong admin';
+}
+}
+?>
